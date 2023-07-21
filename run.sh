2073e2ec83fa8a8036969303c617c179b6208c4d
Dylan Baxter <dylanjaybaxter@gmail.com>
2023-07-21 00:09:16 -0700

    config to run.sh

--- run.sh
+++ run.sh
@@ -87,7 +87,7 @@ while [ "$z" -le "$global_epochs" ]; do
         echo "************************Training Partition ${i}************************"
         update_yaml_field "config_temp.yaml" "data_dir" "${data_dir}/par${i}/"
         update_yaml_field "config_temp.yaml" "experiment" "${experiment_train}_par${i}"
-        if [ "$initial" -eq 0 ]; then
+        if [[ "$initial" == "false" ]]; then
             update_yaml_field "config_temp.yaml" "model" "/workspace/${project_train}/${experiment_train}/weights/last.pt"
         else
             echo "Using config model..."
