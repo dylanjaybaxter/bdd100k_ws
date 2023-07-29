ae8b7d6b2b58cc2b9db3a9adb1d3fa95905d91d0
Dylan Baxter <dylanjaybaxter@gmail.com>
2023-07-28 18:22:46 -0700

    bash newlines

--- processing_docker.sh
+++ processing_docker.sh
@@ -1,6 +1,7 @@
 #!bin/bash
-docker build -f ./Dockerfile -t processingimage .
-docker run -v bdd100k-data:/app/dataset --gpus all \
+docker build -f ./Dockerfile \
     --build-arg dst="/app/dataset/1par_yolo_mots/" \
     --build-arg src="/app/dataset/bdd100k/" \
+     -t processingimage .
+docker run -v bdd100k-data:/app/dataset --gpus all \
     --ipc=host -it processingimage 
