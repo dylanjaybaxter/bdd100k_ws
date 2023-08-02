73e175292d6089d24f280f9d8d87d4e2f2dc21bf
Dylan Baxter <dylanjaybaxter@gmail.com>
2023-08-01 17:36:12 -0700

    fixed workspace name

--- training_docker.sh
+++ training_docker.sh
@@ -1,4 +1,4 @@
 docker build -f ./yolo_training/Dockerfile \
      -t trainingimage .
-docker run -v bdd100k-data:/app/dataset --gpus all \
+docker run -v bdd100k-data:/workspace/dataset --gpus all \
     --ipc=host -it trainingimage 
