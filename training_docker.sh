3aaa82f68b16e808acb551403a71567c11b87585
Dylan Baxter <dylanjaybaxter@gmail.com>
2023-08-02 17:12:21 -0700

    permissions fixes

--- training_docker.sh
+++ training_docker.sh
@@ -4,4 +4,4 @@ docker build -f ./yolo_training/Dockerfile \
      -t trainingimage .
 docker run -v bdd100k-data:/workspace/dataset \
      --gpus all \
-     --ipc=host -it trainingimage 
+     --ipc=host trainingimage 
