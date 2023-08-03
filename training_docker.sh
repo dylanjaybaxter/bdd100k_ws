481ffd6a7bed7959f0c1ac28fb9c57b706a9a005
Dylan Baxter <dylanjaybaxter@gmail.com>
2023-08-02 17:03:18 -0700

    permissions fixes

--- training_docker.sh
+++ training_docker.sh
@@ -1,7 +1,7 @@
 docker build -f ./yolo_training/Dockerfile \
+     --build-arg UID_VAR=$(id -u) \
+     --build-arg GID_VAR=$(id -g) \
      -t trainingimage .
 docker run -v bdd100k-data:/workspace/dataset \
      --gpus all \
-     --build-arg UID_VAR=$(id -u) \
-     --build-arg GID_VAR=$(id -g) \
      --ipc=host -it trainingimage 
