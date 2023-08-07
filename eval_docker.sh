7b22ebb0014cad5d36bb74c3906abe7c7119fe8f
Dylan Baxter <dylanjaybaxter@gmail.com>
2023-08-07 15:44:27 -0700

    Adding evalutation tools

---
+++ eval_docker.sh
@@ -0,0 +1,7 @@
+docker build -f ./yolo_eval/Dockerfile \
+     --build-arg UID_VAR=$(id -u) \
+     --build-arg GID_VAR=$(id -g) \
+     -t evalimage .
+docker run -v bdd100k-data:/workspace/dataset \
+     --gpus all \
+     --ipc=host evalimage 
